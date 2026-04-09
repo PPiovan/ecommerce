cadf75260b3685a96e23f0ff2471658e890bf455
Pablo <pablopiovan94@gmail.com>
2026-04-09 10:26:23 -0300

    fix https mixed content

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -14,7 +15,10 @@ class AppServiceProvider extends ServiceProvider
     }
 
   public function boot(): void
-    {
+    {   
+        if (app()->environment('production')) {
+        URL::forceScheme('https');
+        }
         Gate::before(function ($user, $ability) {
             return $user->hasRole('SUPER ADMIN') ? true : null;
         });
